7efb3d5b11e19739667a7ea6ba1e7666699af62f
apple115 <2567031030@qq.com>
2023-07-10 21:27:14 +0800

    update lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -21,7 +21,14 @@ require("lspconfig").lua_ls.setup{
   capabilities = capabilities,
 }
 
+local cmp_nvim_lsp = require "cmp_nvim_lsp"
 require'lspconfig'.clangd.setup {
+  on_attach = on_attach,
+    capabilities = cmp_nvim_lsp.default_capabilities(),
+    cmd = {
+      "clangd",
+      "--offset-encoding=utf-16",
+    },
 }
 
 require 'lspconfig'.pyright.setup{
