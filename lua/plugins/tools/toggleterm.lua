f7017a11c0f6c2d056b9b90858f731f4e295b4a9
apple115 <2567031030@qq.com>
2024-01-12 15:30:50 +0800

    fix some toggleterm code

--- lua/plugins/tools/toggleterm.lua
+++ lua/plugins/tools/toggleterm.lua
@@ -1,62 +1,26 @@
 return {
 	{
 		"akinsho/toggleterm.nvim",
-		version = "*",
-		config = function()
-			local Terminal = require("toggleterm.terminal").Terminal
-			local lazygit = Terminal:new({
-				cmd = "lazygit",
-				dir = "git_dir",
-				direction = "float",
-				float_opts = {
-					border = "double",
-				},
-				-- function to run on opening the terminal
-				on_open = function(term)
-					vim.cmd("startinsert!")
-					vim.api.nvim_buf_set_keymap(
-						term.bufnr,
-						"n",
-						"q",
-						"<cmd>close<CR>",
-						{ noremap = true, silent = true }
-					)
+		event = "VeryLazy",
+		opts = {
+			persist_mode = false,
+			start_in_insert = true,
+			autochdir = false,
+		},
+		keys = {
+			{
+				"<leader>gl",
+				function()
+					require("toggleterm.terminal").Terminal
+						:new({
+							cmd = "lazygit",
+							hidden = true,
+							direction = "float",
+						})
+						:toggle()
 				end,
-				-- function to run on closing the terminal
-				on_close = function(term)
-					vim.cmd("startinsert!")
-				end,
-			})
-
-			local joshuto = Terminal:new({
-				cmd = "joshuto",
-				direction = "float",
-				float_opts = {
-					border = "double",
-				},
-				on_open = function(term)
-					vim.cmd("startinsert!")
-					vim.api.nvim_buf_set_keymap(
-						term.bufnr,
-						"n",
-						"q",
-						"<cmd>close<CR>",
-						{ noremap = true, silent = true }
-					)
-				end,
-				-- function to run on closing the terminal
-				on_close = function(term)
-					vim.cmd("startinsert!")
-				end,
-			})
-
-			function _lazygit_toggle()
-				lazygit:toggle()
-			end
-			function _joshuto_toggle()
-				joshuto:toggle()
-			end
-			require("toggleterm").setup({})
-		end,
+				desc = "LazyGit",
+			},
+		},
 	},
 }
