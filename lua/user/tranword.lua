ed310699ee94af9f13d6403fbef2f2a40e518a21
apple115 <2567031030@qq.com>
2024-02-09 00:51:25 +0800

    添加一些根据文本类型翻译

--- lua/user/tranword.lua
+++ lua/user/tranword.lua
@@ -1,8 +1,8 @@
 -- 获取当前光标所在位置的单词
 local function getCurrentWord()
-  -- 获取光标所在的行和列
-  local line = vim.fn.line('.')
-  local col = vim.fn.col('.')
+  -- -- 获取光标所在的行和列
+  -- local line = vim.fn.line('.')
+  -- local col = vim.fn.col('.')
 
   -- 使用 expand() 函数获取当前单词
   local currentWord = vim.fn.expand('<cword>')
@@ -59,6 +59,38 @@ local function appendCurrentWord(word)
   vim.fn.cursor(line, col + #word)
 end
 
+-- 翻译当前单词并替换
+-- tRanslationsOftware
+-- translation software
+-- cOnfirmt His wOrd
+-- 根据文本类型进行一些处理
+local function define_processing(words)
+  --如何是python文本
+  if vim.bo.filetype == "python" then
+    --单词中间添加下划线
+    return words:gsub("%s+", "_")
+  end
+  if vim.bo.filetype == "lua" then
+    -- 将每个单词首字母大写
+    local Words = words:gsub("(%a)(%w*)", function(first, rest)
+      return first:upper() .. rest
+    end)
+    local camelWords = Words:gsub("%s+", "")
+    -- 将首字母小写
+    camelWords = camelWords:sub(1, 1):lower() .. camelWords:sub(2)
+    return camelWords
+  end
+  -- if vim.bo.filetype == "rust" then
+  --
+  -- end
+  --
+  -- if vim.bo.filetype == "c" or vim.bo.filetype == "cpp" then
+  --
+  -- end
+  return words
+end
+
+
 function Translate_current_word_and_replace ()
   -- 获取当前光标所在位置的单词
   local currentWord = getCurrentWord()
@@ -66,15 +98,18 @@ function Translate_current_word_and_replace ()
   replaceCurrentWord(return_translation)
 end
 
+--- 翻译定义
 function trans_definition ()
   vim.fn.system("fcitx5-remote -o")
-  local currentWord = vim.fn.input("请输入翻译的内容：")
+  local currentWord = vim.fn.input("input:")
   --将fcitx输入法切换到中文模式
   local return_translation=translation(currentWord)
-  appendCurrentWord(return_translation)
+  local return_words=define_processing(return_translation)
+  appendCurrentWord(return_words)
   vim.fn.system("fcitx5-remote -c")
   vim.fn.feedkeys("a")
 end
+
 
 vim.cmd([[
   imap <c-d> <esc>:lua trans_definition()<CR>
